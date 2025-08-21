9dc0cc02e158c2a1c4b67375242a2368b10429d7
Maddy <41277001+MaddyBleh@users.noreply.github.com>
2025-08-22 11:40:33 +1200

    Parabola Temperature Fluctuation added, refactored Sensor.cs
    Internal test project has been created, but added to the .gitignore.

--- SensorLibrary/Sensor.cs
+++ SensorLibrary/Sensor.cs
@@ -4,10 +4,7 @@ public class Sensor : ISensor
 {
     private double _minTemp;
     private double _maxTemp;
-    private TempRange _range;
-
-    private int _step, _maxSteps;
-    private bool _rising;
+    public TempRange tempRange;
 
     public double currentTemp;
 
@@ -16,31 +13,27 @@ public class Sensor : ISensor
     /// </summary>
     /// <param name="minTemp">The lowest temperature allowed (can be negative)</param>
     /// <param name="maxTemp">The highest temperature allowed (can be negative, must be higher than minTemp)</param>
-    /// <param name="stepsPerCycle">The number of steps required to go from <paramref name="minTemp"/> to <paramref name="maxTemp"/>. Defaults to 100.</param>
     public Sensor(double minTemp, double maxTemp, int stepsPerCycle = 100)
     {
         _minTemp = minTemp;
         _maxTemp = maxTemp;
-        _step = 0;
-        _maxSteps = stepsPerCycle;
-        _rising = true;
         currentTemp = minTemp;
-        _range = new TempRange("Custom", minTemp, maxTemp);
+        tempRange = new TempRange("Custom", minTemp, maxTemp);
     }
 
     /// <summary>
     /// A Sensor that uses a specific temperature range.
     /// </summary>
     /// <param name="range">The <see cref="TempRange"/> that determines the min and max temperatures.</param>
-    /// <param name="stepsPerCycle">The number of steps required to go from min to max. Defaults to 100.</param>
     public Sensor(TempRange range, int stepsPerCycle = 100) : this(range.Min, range.Max, stepsPerCycle)
     {
-        _range = range;
+        tempRange = range;
     }
 
 
     public double Fluctuate(double min, double max, Func<double, double, double, double> fluctuation)
     {
+        // Call the given fluctuation graph
         currentTemp = fluctuation(currentTemp, min, max);
         return currentTemp;
     }
---
+++ SensorLibrary/TempFluctuations.cs
@@ -0,0 +1,38 @@
+﻿namespace SensorLib.Fluctuations;
+
+public class TempFluctuations
+{
+    private int _step = 0;
+    private int _maxSteps = 100;
+    private bool _rising = true;
+
+    public double Parabola(double current, double min, double max)
+    {
+        // Keep step between the range of [-1, 1]
+        double x = _step / (double)_maxSteps * 2 - 1; // x between [-1, 1]
+
+        // Parabola function, scaled to [min, max]
+        double parabola = -(x * x) + 1;
+        current = min + parabola * (max - min);
+
+        // Increment or decrement step depending on direction
+        if (_rising)
+            _step++;
+        else
+            _step--;
+
+        // Flip direction at the ends
+        if (_step >= _maxSteps)
+        {
+            _rising = false;
+            _step = _maxSteps;
+        }
+        else if (_step <= 0)
+        {
+            _rising = true;
+            _step = 0;
+        }
+
+        return Math.Round(current, 3, MidpointRounding.AwayFromZero);
+    }
+}
